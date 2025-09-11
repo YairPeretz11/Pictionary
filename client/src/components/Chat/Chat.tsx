5703e8e8771f9bf8c3a9331911f452c91691a404
Yair Peretz <yairperetz11@gmail.com>
2025-09-11 20:01:37 +0300

    basic chat

--- client/src/components/Chat/Chat.tsx
+++ client/src/components/Chat/Chat.tsx
@@ -1,7 +1,50 @@
-import React, { FC, useState } from "react";
+import React, { useState } from "react";
 import "./Chat.css";
 
+interface Message {
+  id: number;
+  text: string;
+}
+
 const Chat = () => {
-  
+  const [messages, setMessage] = useState<Message[]>([]);
+  const [input, setInput] = useState<string>("");
+
+  const handleSend = (event: any) => {
+    setMessage((prevMessages) => [
+      ...prevMessages,
+      { id: Date.now(), text: input },
+    ]);
+    setInput("");
+
+    event.preventDefault();
+  };
+
+  return (
+    <div className="chat-wrapper">
+      <div className="messages">
+        <ul>
+          {messages.map((message) => (
+            <li key={message.id} className="message">
+              {message.text}
+            </li>
+          ))}
+        </ul>
+      </div>
+      <form onSubmit={handleSend} className="chat-input-area">
+        <input
+          type="text"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          placeholder="Type Your Message Here"
+          className="chat-input"
+        />
+        <button disabled={!input.trim()} type="submit" className="chat-button">
+          Send
+        </button>
+      </form>
+
+    </div>
+  );
 };
 export default Chat;
